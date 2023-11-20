3bb3bab13548cf2e992d642cedd4bd41a3e3089a
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2023-11-20 11:49:02 +0200

    First version

---
+++ app/forms.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from wtforms import DecimalField, StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+class MixtureForm(FlaskForm):
+  pressure = DecimalField(u'Paine pullossa bar', validators=[DataRequired()])
+  o2_bottle_percentage = DecimalField(u'O2-% pullossa', validators=[DataRequired()])
+  fill_amount = DecimalField(u'Täytettävä määrä bar', validators=[DataRequired()])
+  o2_fill_percentage = DecimalField(u'O2 täyttö-%', validators=[DataRequired()])
+  used_po2 = DecimalField(u'Käytetty PO2', validators=[DataRequired()])
+
+class FeedbackForm(FlaskForm):
+  name = StringField(u'Nimi')
+  email = StringField(u'Sähköpostiosoite')
+  message = TextAreaField(u'Viesti', validators=[DataRequired()])
---
+++ app/views.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+
+from flask import render_template, request, Response, redirect
+from app import app
+from app.forms import *
+
+from email.mime.text import MIMEText
+import smtplib
+
+import sys, os
+
+import datetime
+
+@app.route('/', methods=['GET', 'POST'])
+def mixture():
+  form = MixtureForm()
+  if form.validate_on_submit():
+#    print(form.file.data.filename, file=sys.stderr)
+
+    pressure_after = form.pressure.data + form.fill_amount.data
+    end_mixture = (form.pressure.data / pressure_after * form.o2_bottle_percentage.data) + (form.fill_amount.data / pressure_after * form.o2_fill_percentage.data)
+    mod = (form.used_po2.data - (end_mixture / 100)) / (end_mixture / 1000)
+
+    return render_template('result.html', pressure_after=pressure_after, end_mixture=end_mixture, mod=mod)
+
+  return render_template('form.html', form=form)
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+  form = FeedbackForm()
+  if form.validate_on_submit():
+    body = u"""Viitemuuntimen palautelomakkeella tullut viesti:
+
+Nimi: %s
+Sahkopostiosoite: %s
+Viesti:
+%s""" % (form.name.data, form.email.data, form.message.data)
+    msg = MIMEText(body)
+    msg['From'] = app.config['EMAIL_FROM']
+    msg['To'] = app.config['FEEDBACK_TO']
+    msg['Subject'] = "Viitemuunninpalaute"
+
+    server = smtplib.SMTP(app.config['SMTP_SERVER'])
+    server.send_message(msg)
+    return redirect('/success')
+
+  return render_template('feedback.html', form=form)
+
+@app.route('/success')
+def success():
+  return render_template('success.html')
